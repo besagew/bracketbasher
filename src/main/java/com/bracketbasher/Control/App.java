11f32100df1942ed6d08c29e23d64f8309f79e96
calvin <calvingoldenreal@gmail.com>
2025-04-08 17:33:24 -0400

    list app requirements

--- src/main/java/com/bracketbasher/Control/App.java
+++ src/main/java/com/bracketbasher/Control/App.java
@@ -13,5 +13,9 @@ import javafx.stage.Stage;
 public final class App {
 
     public static void main(String[] args) {
+        // This function should wait for a user to input a CSV string
+        // Once entered, prompt them with pairs of choices and ask them if they prefer choice 1 or 2
+        // Recursively do this until the entire list is filled
+        // Once list is filled, create a new BracketDisplay with the list
     }
 }
--- src/main/java/com/bracketbasher/Control/TestApp.java
+++ src/main/java/com/bracketbasher/Control/TestApp.java
@@ -7,9 +7,6 @@ import com.bracketbasher.View.BracketDisplay;
 
 import javafx.stage.Stage;
 
-/**
- * Hello world!
- */
 public final class TestApp {
 
     public static void main(String[] args) {
